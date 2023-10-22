75410372efcdb36897b3f3813493a9a62893d4df
emmy <emmy.schmidt@hotmail.com>
2023-10-22 19:17:52 +1100

    Attempted merge explore_lite with brick detection
    not working yet

--- src/brick_search.py
+++ src/brick_search.py
@@ -20,6 +20,9 @@ import actionlib
 import random
 import copy
 from threading import Lock
+from visualization_msgs.msg import Marker
+from sensor_msgs.msg import LaserScan
+
 
 
 
@@ -89,12 +92,28 @@ class BrickSearch:
 
         # Reinitialise AMCL
         global_localization_service_client = rospy.ServiceProxy('global_localization', Empty)
-        empty = global_localization_service_client()
+        #empty = global_localization_service_client()
         rospy.sleep(0.5)
 
         # Subscribe to "amcl_pose" to get pose covariance
         self.amcl_pose_sub_ = rospy.Subscriber("amcl_pose", PoseWithCovarianceStamped, self.amcl_pose_callback, queue_size=1)
 
+        self.marker_pub_ = rospy.Publisher('brick_marker', Marker, queue_size=10)
+
+        self.laser_sub_ = rospy.Subscriber("/scan", LaserScan, self.laser_callback, queue_size=1)
+        self.latest_scan_ = None
+
+    def laser_callback(self, data):
+        self.latest_scan_ = data
+
+    def get_distance_in_front(self):
+        # Assuming laser scanner is front-facing and has a 180-degree field of view
+        # If the robot is oriented such that 0-degree is front, then:
+        if self.latest_scan_ is None:
+            return None  # No data received yet
+
+        front_distance = self.latest_scan_.ranges[len(self.latest_scan_.ranges) // 2]
+        return front_distance if front_distance < self.latest_scan_.range_max else None
 
     def get_pose_2d(self):
 
@@ -137,6 +156,8 @@ class BrickSearch:
     def image_callback(self, image_msg):
         # Use this method to identify when the brick is visible
 
+        print("start image_callback")
+
         # The camera publishes at 30 fps, it's probably a good idea to analyse images at a lower rate than that
         if self.image_msg_count_ < 15:
             self.image_msg_count_ += 1
@@ -147,17 +168,79 @@ class BrickSearch:
         # Copy the image message to a cv_bridge image
         image = self.cv_bridge_.imgmsg_to_cv2(image_msg)
 
+        # Convert to RGB format
+        image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+
+        # Reduce image height to 2%
+        height, width, _ = image.shape
+
+        top = int(height*0.49)
+        bottom = int(height*0.51)
+
+        image = image[top:bottom, :] # Slice image
+
+        # Specify 'Red' Colour Range
+        lower_range_red = np.array([0, 0, 100], dtype=np.uint8)
+        upper_range_red = np.array([40, 40, 255], dtype=np.uint8)
+
+        # Create a bit mask that specifies pixels in the correct colour range
+        bit_mask = cv.inRange(image, lower_range_red, upper_range_red)
+
+        red_present = cv.countNonZero(bit_mask) > 0
+
+        self.brick_found_ = red_present
+        
         # You can set "brick_found_" to true to signal to "mainLoop" that you have found a brick
         # You may want to communicate more information
         # Since the "image_callback" and "main_loop" methods can run at the same time you should protect any shared variables
         # with a mutex
         # "brick_found_" doesn't need a mutex because it's an atomic
 
-
-
-
         rospy.loginfo('image_callback')
         rospy.loginfo('brick_found_: ' + str(self.brick_found_))
+
+        if self.brick_found_:
+            distance_to_brick = self.get_distance_in_front()
+            if distance_to_brick is not None:
+                # Get the robot's current pose
+                pose_2d = self.get_pose_2d()
+
+                # Calculate the position of the brick relative to the robot's current position
+                brick_position = Pose2D()
+                brick_position.x = pose_2d.x + distance_to_brick * math.cos(pose_2d.theta)
+                brick_position.y = pose_2d.y + distance_to_brick * math.sin(pose_2d.theta)
+                brick_position.theta = pose_2d.theta
+
+                # Create a marker for the brick's position
+                marker = Marker()
+                marker.header.frame_id = "map"
+                marker.header.stamp = rospy.Time.now()
+                marker.ns = "brick_marker"
+                marker.id = 0
+                marker.type = Marker.CUBE
+                marker.action = Marker.ADD
+
+                marker.pose.position.x = brick_position.x
+                marker.pose.position.y = brick_position.y - 0.065
+                marker.pose.position.z = 0.094
+                marker.pose.orientation.x = 0.0
+                marker.pose.orientation.y = 0.0
+                marker.pose.orientation.z = 0.0
+                marker.pose.orientation.w = 1.0
+
+                marker.scale.x = 0.3  
+                marker.scale.y = 0.3
+                marker.scale.z = 0.2
+                marker.color.a = 1.0
+                marker.color.r = 1.0
+                marker.color.g = 0.0
+                marker.color.b = 0.0
+
+                # Publish the marker
+                self.marker_pub_.publish(marker)
+
+                rospy.loginfo('Brick_Marker Set')
+
 
     def main_loop(self):
 
